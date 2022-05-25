456c51a5c0d0a6de43d7e1a7e8594b0c39a2cba8
Tiago Januario <2211907@my.ipleiria.pt>
2022-05-25 12:14:20 +0100

    teste

--- Projeto_PWS/views/site/Login.php
+++ Projeto_PWS/views/site/Login.php
@@ -1,12 +1,11 @@
 <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity = "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin = "anonymous" ></script >
 
-  <link rel = "stylesheet" href = "public/css/Login.css" >
-
-    <div id="content">
-        <div id = "Login" >
-            <h1 id = "login-text" > Login</h1 >
-            <div >
-                <form id = "formulario" action = "router.php?c=login&a=auth" method = "post" >
+<link rel = "stylesheet" href = "public/css/Login.css" >
+<div id="content">
+    <div id = "Login" >
+        <h1 id = "login-text" > Login</h1 >
+        <div >
+            <form id = "formulario" action = "router.php?c=login&a=auth" method = "post" >
 
                     <input class="inputs" type = "text" name = "nome" placeholder="Username"><br >
 
