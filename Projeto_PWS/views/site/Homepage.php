73be446eb1f334b0d9fc0366e6ef39d3fe8a66bf
Tiago Januário <2211907@my.ipleiria.pt>
2022-06-13 21:36:43 +0100

    Esconder navbar homepage

--- Projeto_PWS/views/site/Homepage.php
+++ Projeto_PWS/views/site/Homepage.php
@@ -14,8 +14,11 @@
             var i;
             for (i=0; i<text.length; i++)
             {
-                text[i].style.visibility = "hidden";
+                text[i].style.display = "none";
             }
+
+            var button = document.getElementById("hideNavBar");
+            button.style.transform = "rotate(90deg)";
 
             x = 1;
         }
@@ -25,8 +28,11 @@
             var i;
             for (i=0; i<text.length; i++)
             {
-                text[i].style.visibility = "visible";
+                text[i].style.display = "inline";
             }
+
+            var button = document.getElementById("hideNavBar");
+            button.style.transform = "rotate(0deg)";
             x = 0;
         }
 
