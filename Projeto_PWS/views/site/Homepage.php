51e46d45000a8a818aec3bd514ef67d3787bc63a
Tiago Januário <2211907@my.ipleiria.pt>
2022-06-17 01:06:19 +0100

    HomePage

--- Projeto_PWS/views/site/Homepage.php
+++ Projeto_PWS/views/site/Homepage.php
@@ -1,30 +1,37 @@
 <?php
 ?>
-<br>
-<br>
+
+<link rel = "stylesheet" href = "public/css/Homepage.css" >
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script src="public/js/NavBar.js"></script>
+
+<div id="buttons" >
+
+    <button type="button" onclick="HideNavBar()" id="hideNavBar"><i class="fa-solid fa-bars"></i></button>
+    <br> <br> <br>
 
 <a href="router.php?c=login&a=logout"
-   class="btn btn-info" role="button">Logout</a>
+   class="btn btn-info" role="button"><i class="fa-solid fa-arrow-right-from-bracket"></i> <span class="NavText"> Logout</span></a>
+
 
 <?php
 if($_SESSION['role']!=3){
     echo '
         <a href="router.php?c=users&a=index"
-        class="btn btn-info" role="button">Utilizadores</a>
+        class="btn btn-info" role="button"><i class="fa-solid fa-user"></i><span class="NavText">Utilizadores</span></a>
         <a href="router.php?c=empresas&a=index"
-        class="btn btn-info" role="button">Empresas</a>
+        class="btn btn-info" role="button"><i class="fa-solid fa-building"></i> <span class="NavText">Empresas</span></a>
         <a href="router.php?c=ivas&a=index"
-        class="btn btn-info" role="button">Ivas</a>
+        class="btn btn-info" role="button"><i class="fa-solid fa-percent"></i><span class="NavText">Ivas</span></a>
         <a href="router.php?c=produtos&a=index"
-        class="btn btn-info" role="button">Produtos</a>
+        class="btn btn-info" role="button"><i class="fa-solid fa-cart-shopping"></i> <span class="NavText">Produtos</span></a>
         <a href="router.php?c=faturas&a=index"
-        class="btn btn-info" role="button">Histórico de faturas</a>
+        class="btn btn-info" role="button"><i class="fa-solid fa-clock-rotate-left"></i><span class="NavText"> Histórico de faturas</span></a>
         <a href="router.php?c=faturas&a=create"
-        class="btn btn-info" role="button">Nova fatura</a>';
+        class="btn btn-info" role="button"><i class="fa-solid fa-file-circle-plus"></i> <span class="NavText">Nova fatura</span></a>';
 }
 else
     echo '<a href="router.php?c=faturas&a=cliente&id=' . $_SESSION['userId'] . '"
         class="btn btn-info" role="button">Minhas faturas</a>';
 ?>
-<br>
-<br>
+</div>
