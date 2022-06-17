625eb5a5ad0ee057c0be1629e9341b542763b470
Tiago Januário <2211907@my.ipleiria.pt>
2022-06-17 01:27:57 +0100

    Pagina Utilizadores

--- Projeto_PWS/views/site/Users.php
+++ Projeto_PWS/views/site/Users.php
@@ -1,14 +1,59 @@
 <?php
 $users = $params;
 
-?>
+?><!--
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+!-->
 <script src="public/js/FaturasCreate.js"></script>
 
 <link rel = "stylesheet" href = "public/css/BarraPesquisa.css" >
 
-<h2>Utilizadores</h2>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel = "stylesheet" href = "public/css/Homepage.css" >
+<link rel="stylesheet" href="public/css/table.css">
+<script src="public/js/NavBar.js"></script>
+
+<div style="justify-content: space-between">
+
+<div id="navBar" style="float: left">
+
+<div id="buttons" style="margin: 0px">
+
+    <button type="button" onclick="HideNavBar()" id="hideNavBar"><i class="fa-solid fa-bars"></i></button>
+    <br> <br> <br>
+
+    <a href="router.php?c=login&a=logout"
+       class="btn btn-info" role="button"><i class="fa-solid fa-arrow-right-from-bracket"></i> <span class="NavText"> Logout</span></a>
+
+
+    <?php
+    if($_SESSION['role']!=3){
+        echo '
+        <a href="router.php?c=users&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-user"></i><span class="NavText">Utilizadores</span></a>
+        <a href="router.php?c=empresas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-building"></i> <span class="NavText">Empresas</span></a>
+        <a href="router.php?c=ivas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-percent"></i><span class="NavText">Ivas</span></a>
+        <a href="router.php?c=produtos&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-cart-shopping"></i> <span class="NavText">Produtos</span></a>
+        <a href="router.php?c=faturas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-clock-rotate-left"></i><span class="NavText"> Histórico de faturas</span></a>
+        <a href="router.php?c=faturas&a=create"
+        class="btn btn-info" role="button"><i class="fa-solid fa-file-circle-plus"></i> <span class="NavText">Nova fatura</span></a>';
+    }
+    else
+        echo '<a href="router.php?c=faturas&a=cliente&id=' . $_SESSION['userId'] . '"
+        class="btn btn-info" role="button">Minhas faturas</a>';
+    ?>
+</div>
+
+</div>
+
+
+    <div style="width: 79%; float: left; margin-left: 10px;">
+<h2 id="caixa" >Utilizadores</h2>
 <h2 class="top-space"></h2>
 <div class="row">
     <div class="col-sm-12">
@@ -48,18 +93,18 @@ $users = $params;
                     <td>
                         <?php if($user->role == 3) {
                             echo "<a href='router.php?c=faturas&a=cliente&id=$user->id'
-                           class='btn btn-info' role='button'>Detalhes</a>";
+                           class='btn btn-info' role='button'><i class='fa-solid fa-eye' style='color: black'></i></a>";
                         }
                         ?>
 
                         <?php if($_SESSION['userId']==$user->id || $_SESSION['role']==1) {
                             echo "<a href='router.php?c=users&a=edit&id=$user->id'
-                           class='btn btn-info' role='button'>Editar</a>";
+                           class='btn btn-info' role='button'><i class='fa-solid fa-pencil' style='color: black'></i></a>";
                         }
                         ?>
                         <?php if($user->role !=1 && $_SESSION['role']==1) {
                             echo "<a href='router.php?c=users&a=delete&id=$user->id'
-                           class='btn btn-warning' role='button'>Apagar</a>";
+                           class='btn btn-warning' role='button'><i class='fa-solid fa-trash-can' style='color: black'></i></a>";
                         }
                         ?>
                     </td    >
@@ -68,7 +113,7 @@ $users = $params;
             </tbody>
         </table>
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-6" id="caixa">
         <h3>Criar um novo utilizador</h3>
         <p>
             <a href="router.php?c=users&a=create" class="btn btn-info"
@@ -77,6 +122,8 @@ $users = $params;
                role="button">Homepage</a>
         </p>
     </div>
+</div>
+</div>
 </div>
 
 
