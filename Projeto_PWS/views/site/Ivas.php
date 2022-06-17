08de5d83fb90c34687341f369e9dfe02f4de7ad3
Tiago Januário <2211907@my.ipleiria.pt>
2022-06-17 01:40:03 +0100

    Paginas Iva

--- Projeto_PWS/views/site/Ivas.php
+++ Projeto_PWS/views/site/Ivas.php
@@ -1,11 +1,53 @@
 <?php
 $ivas = $params;
-?>
+?><!--
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+!-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel = "stylesheet" href = "public/css/Homepage.css" >
+    <link rel="stylesheet" href="public/css/table.css">
+    <script src="public/js/NavBar.js"></script>
+
+    <div style="justify-content: space-between">
+
+        <div id="navBar" style="float: left">
+
+            <div id="buttons" style="margin: 0px">
+
+                <button type="button" onclick="HideNavBar()" id="hideNavBar"><i class="fa-solid fa-bars"></i></button>
+                <br> <br> <br>
+
+                <a href="router.php?c=login&a=logout"
+                   class="btn btn-info" role="button"><i class="fa-solid fa-arrow-right-from-bracket"></i> <span class="NavText"> Logout</span></a>
 
 
-    <h2>Ivas</h2>
+                <?php
+                if($_SESSION['role']!=3){
+                    echo '
+        <a href="router.php?c=users&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-user"></i><span class="NavText">Utilizadores</span></a>
+        <a href="router.php?c=empresas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-building"></i> <span class="NavText">Empresas</span></a>
+        <a href="router.php?c=ivas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-percent"></i><span class="NavText">Ivas</span></a>
+        <a href="router.php?c=produtos&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-cart-shopping"></i> <span class="NavText">Produtos</span></a>
+        <a href="router.php?c=faturas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-clock-rotate-left"></i><span class="NavText"> Histórico de faturas</span></a>
+        <a href="router.php?c=faturas&a=create"
+        class="btn btn-info" role="button"><i class="fa-solid fa-file-circle-plus"></i> <span class="NavText">Nova fatura</span></a>';
+                }
+                else
+                    echo '<a href="router.php?c=faturas&a=cliente&id=' . $_SESSION['userId'] . '"
+        class="btn btn-info" role="button">Minhas faturas</a>';
+                ?>
+            </div>
+
+        </div>
+
+        <div style="width: 79%; float: left; margin-left: 10px;">
+    <h2 id="caixa">Ivas</h2>
     <h2 class="top-space"></h2>
     <div class="row">
         <div class="col-sm-12">
@@ -36,10 +78,10 @@ $ivas = $params;
                             }?></td>
                         <td>
                             <a href="router.php?c=ivas&a=edit&id=<?=$iva->id ?>"
-                               class="btn btn-info" role="button">Editar</a>
+                               class="btn btn-info" role="button"><i class='fa-solid fa-pencil' style='color: black'></i></a>
                             <?php if($iva->id > 3){
                             ?>   <a href='router.php?c=ivas&a=delete&id=<?$iva->id?>'
-                                 class='btn btn-warning' role='button'>Apagar</a>
+                                 class='btn btn-warning' role='button'><i class='fa-solid fa-trash-can' style='color: black'></i></a>
                             <?php } ?>
                         </td>
                     </tr>
@@ -57,6 +99,9 @@ $ivas = $params;
             </p>
         </div>
     </div>
+        </div>
+    </div>
+
 
 
 <?php
--- Projeto_PWS/views/site/Produtos.php
+++ Projeto_PWS/views/site/Produtos.php
@@ -3,11 +3,55 @@ $produtos = $params;
 
 if($_SESSION['role'] == 3)
     header('location: router.php?c=site&a=index');
-?>
+?><!--
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+!-->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel = "stylesheet" href = "public/css/Homepage.css" >
+<link rel="stylesheet" href="public/css/table.css">
+<script src="public/js/NavBar.js"></script>
+
+<div style="justify-content: space-between">
+
+    <div id="navBar" style="float: left">
+
+        <div id="buttons" style="margin: 0px">
+
+            <button type="button" onclick="HideNavBar()" id="hideNavBar"><i class="fa-solid fa-bars"></i></button>
+            <br> <br> <br>
+
+            <a href="router.php?c=login&a=logout"
+               class="btn btn-info" role="button"><i class="fa-solid fa-arrow-right-from-bracket"></i> <span class="NavText"> Logout</span></a>
 
 
+            <?php
+            if($_SESSION['role']!=3){
+                echo '
+        <a href="router.php?c=users&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-user"></i><span class="NavText">Utilizadores</span></a>
+        <a href="router.php?c=empresas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-building"></i> <span class="NavText">Empresas</span></a>
+        <a href="router.php?c=ivas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-percent"></i><span class="NavText">Ivas</span></a>
+        <a href="router.php?c=produtos&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-cart-shopping"></i> <span class="NavText">Produtos</span></a>
+        <a href="router.php?c=faturas&a=index"
+        class="btn btn-info" role="button"><i class="fa-solid fa-clock-rotate-left"></i><span class="NavText"> Histórico de faturas</span></a>
+        <a href="router.php?c=faturas&a=create"
+        class="btn btn-info" role="button"><i class="fa-solid fa-file-circle-plus"></i> <span class="NavText">Nova fatura</span></a>';
+            }
+            else
+                echo '<a href="router.php?c=faturas&a=cliente&id=' . $_SESSION['userId'] . '"
+        class="btn btn-info" role="button">Minhas faturas</a>';
+            ?>
+        </div>
+
+    </div>
+
+
+    <div style="width: 79%; float: left; margin-left: 10px;">
 <h2>Produtos</h2>
 <h2 class="top-space"></h2>
 <div class="row">
