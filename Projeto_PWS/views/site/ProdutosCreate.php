c404e36350d7c12dd47b4b5ed6f010a70e210521
Tiago Januário <2211907@my.ipleiria.pt>
2022-06-17 03:39:28 +0100

    Pagina ProdutosCreate

--- Projeto_PWS/views/site/ProdutosCreate.php
+++ Projeto_PWS/views/site/ProdutosCreate.php
@@ -3,27 +3,32 @@ $ivas = Iva::all();
 if($_SESSION['role'] == 3)
     header('location: router.php?c=site&a=index');
 ?>
-<h1>Criar Produto:</h1>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<link rel="stylesheet" href="public/css/Edit.css">
+<div id="content">
+
+<h1 style="text-align: center">Criar Produto</h1>
 
 <form action=router.php?c=produtos&a=store method="post">
-    <div>
-        <label>REFERÊNCIA:</label><br>
+    <div class="informacoes">
+        <label>Referência:</label><br>
         <input type='text' class="form-control" name='referencia' placeholder="">
     </div>
-    <div>
-        <label>DESCRIÇÃO:</label>
+    <div class="informacoes">
+        <label>Descrição:</label><br>
         <input type='text' class="form-control" name='descricao' placeholder="">
     </div>
-    <div>
-        <label>PREÇO:</label>
+    <div class="informacoes">
+        <label>Preço:</label><br>
         <input type='number' class="form-control" min="0" step="0.01" name='preco' placeholder="">
     </div>
-    <div>
-        <label>STOCK:</label>
+    <div class="informacoes">
+        <label>Stock:</label><br>
         <input type='number' class="form-control" min="0" name='stock' placeholder="">
     </div>
-    <div>
-        <label>IVA:</label>
+    <div class="informacoes">
+        <label>IVA:</label><br>
         <select name="iva_id">
             <?php
             foreach ($ivas as $iva){
@@ -32,7 +37,8 @@ if($_SESSION['role'] == 3)
             <?php } }?>
         </select>
     </div>
-    <button type="submit" class="btn btn-info">Criar</button>
+    <button type="submit" class="btn btn-info" id="submit">Criar</button>
 </form>
 
-<a href="router.php?c=produtos&a=index" class="btn btn-info" role="button">Voltar</a>
+<a href="router.php?c=produtos&a=index" class="btn btn-info" role="button"><i class="fa-solid fa-arrow-left" id="voltar"></i></a>
+</div>
